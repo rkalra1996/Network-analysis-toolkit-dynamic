03e7bc62ad54b80ceb1ebc95c576e83a0171088d
sumit <sumit.nautiyal@gmail.com>
2019-09-17 11:38:48 +0530

    realationship creation added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
-(function(){
-
-    dataModule.getGraphData(function(data) {
+(function () {
+    dataModule.getGraphData(function (data) {
         if (data) {
             console.log('data in index.js', data);
 
             // StartLoop(data.data);
-        }
-        else if (data === undefined) {
+        } else if (data === undefined) {
             console.log('An error occured while readingdata in the index.js')
         }
     })
@@ -15,7 +13,7 @@
         let index = 0;
         let totalIterations = dataToLoop.length;
 
-        let interval = window.setInterval(function(){
+        let interval = window.setInterval(function () {
             console.log('printing', dataToLoop[index]);
             index += 1;
             if (index >= totalIterations) {
@@ -26,4 +24,41 @@
             }
         }, 5000)
     }
+    var w = 300;
+    var h = 300;
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+    relationshipModule.createRelation({
+        svgelem: svg,
+        source: 2,
+        target: 1,
+        weight: 0.75,
+        x1: 100,
+        y1: 100,
+        x2: 200,
+        y2: 100
+    });
+    var i = 2;
+    // setInterval(function () {
+    //     var counter = i++;
+    //     console.log(counter)
+    //     counter = counter / 4;
+    //     relationshipModule.createRelation({
+    //         svgelem: svg,
+    //         source: 2,
+    //         target: 1,
+    //         direction: 'OUT',
+    //         weight: counter,
+    //         x1: 100,
+    //         y1: 100,
+    //         x2: 200,
+    //         y2: 100
+    //     });
+    // }, 3000)
+
+
+
 })();
---
+++ js/relationship/index.js
@@ -0,0 +1,50 @@
+var relationshipModule = (function () {
+    var arrowstatus = false
+    var arrowHead = function (svg) {
+        arrowstatus = true;
+        svg.append("svg:defs").append("svg:marker")
+            .attr("id", "triangle")
+            .attr("refX", 6)
+            .attr("refY", 6)
+            .attr("markerWidth", 30)
+            .attr("markerHeight", 30)
+            .attr("orient", "auto")
+            .append("path")
+            .attr("d", "M 0 0 12 6 0 12 3 6")
+            .style("fill", "black");
+    }
+    var lineCreation = function (svg, data) {
+        var weight = data.weight || 1;
+        var x1 = data.x1;
+        var x2 = data.x2;
+        var y1 = data.y1;
+        var y2 = data.y2;
+
+        svg.append("line")
+            .attr("x1", x1)
+            .attr("y1", y1)
+            .attr("x2", x2)
+            .attr("y2", y2)
+            .attr("stroke-width", weight)
+            .attr("stroke", "black");
+        d3.select("line").attr("marker-end", null)
+            .attr("marker-end", "url(#triangle)");
+    }
+    var createRelation = function (data) {
+        var source = data.source;
+        var target = data.target
+        var svg = data.svgelem;
+        if (!source && !target && !data.x1 && !data.y1 && !data.x2 && !data.y2) {
+            var err = new Error("please proved source and target and coordinates");
+            throw err;
+        }
+        // if (!arrowstatus)
+        arrowHead(svg);
+        lineCreation(svg, data);
+
+
+    }
+    return {
+        createRelation: createRelation
+    }
+})();
